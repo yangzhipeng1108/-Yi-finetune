71d71e75446f9a9618bba9bd3fa8e773bcf7638b
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-11-17 14:49:56 +0800

    Update and rename run.sh to run_m34Y_f.sh

---
+++ run_m34Y_f.sh
@@ -0,0 +1,19 @@
+deepspeed --hostfile  hostfile  train_adam.py \
+                --train_path  /root/llama/llama2-lora-fine-tuning/data/Belle_open_source_1M.json  \
+                --model_name_or_path /root/llama/rl_dpo/Yi-34B-200K \
+                --per_device_train_batch_size 16 \
+                --max_len 1024 \
+                --max_src_len 786 \
+                --learning_rate 1e-4 \
+                --weight_decay 0.1 \
+                --num_train_epochs 1 \
+                --gradient_accumulation_steps 8 \
+                --warmup_ratio 0.1 \
+                --mode baichuan2 \
+                --train_type freeze \
+                --freeze_module_name "layers.59.,layers.58.,layers.57.,layers.56." \
+                --seed 1234 \
+                --ds_file ds_zero3_no_offload.json  \
+                --gradient_checkpointing \
+                --show_loss_step 20 \
+                --output_dir ./output-bc13
